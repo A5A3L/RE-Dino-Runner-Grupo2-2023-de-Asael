eb767ecf78c2630607d40e4813bc6a5a82fa545b
A5A3L <sossatorrezasael@gmail.com>
2023-05-04 03:42:21 -0600

    class 6 potenciadores

--- dino_runner/components/text_utils.py
+++ dino_runner/components/text_utils.py
@@ -20,5 +20,3 @@ def get_centered_message(message, width=SCREEN_WIDTH//2, height=SCREEN_HEIGHT//2
     text_rect = text.get_rect()
     text_rect.center = (width, height)
     return text, text_rect
-    
-    
